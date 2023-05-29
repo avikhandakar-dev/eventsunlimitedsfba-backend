f4701ef7ca15a8ad22a8f964aa74a690002f08da
avikhandakar-dev <avikhandakar@gmail.com>
2023-05-29 12:39:08 +0600

    dashbnoard api update

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: "https://eventsunlimitedsfba-backend-production.up.railway.app/admin",
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
